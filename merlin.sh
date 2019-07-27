55bdcdd9d63a1e856e230e82158100ecb82dd77d
hiyoi <hiyoi@qq.com>
2019-07-27 19:18:53 +0800

    Update merlin.sh

--- merlin.sh
+++ merlin.sh
@@ -64,9 +64,9 @@ iptables -t nat -A PREROUTING -s 192.168.50.0/25 -p tcp -j SSTCP >/dev/null 2>&1
 iptables -t mangle -A PREROUTING -s  192.168.50.0/25 -j SSUDP >/dev/null 2>&1
 
 if [ $(iptables-save -t nat|grep SSTCP|wc -l) -gt 1 ]; then
-	echo "规则生效"
-	echo "强制转发ip为 192.168.50.2～192.168.50.126 设备的全局流量到ss"
-	echo "规则重启路由器后失效"
+	echo "succeed!"
+	echo "forward data from 192.168.50.2～192.168.50.126 to ss"
+	echo "How to fix it? Just reboot your router!"
 else
-	echo "规则未生效"
+	echo "failed!"
 fi
