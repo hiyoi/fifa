15d5bdf1a84f94bf99570a500a678e17425ae1fb
alex <alex@hiyoi.com>
2019-09-26 00:08:47 +0800

    fix bug

--- merlin.sh
+++ merlin.sh
@@ -60,14 +60,6 @@ get_wan0_cidr(){
 }
 
 load_tproxy
-# clear rules before 
-iptables -t nat -F
-iptables -t nat -X
-iptables -t mangle -F
-iptables -t mangle -X
-iptables -P INPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -P OUTPUT ACCEPT
 
 # start two Chain
 iptables -t nat -N SSTCP
